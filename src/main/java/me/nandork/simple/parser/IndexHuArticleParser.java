c0d44ee845b83e67371a6f71e6dbc17f06c866b6
Nandor Kracser <bonifaido@gmail.com>
2014-08-16 21:36:42 +0200

    more precise parser, fixed typos

--- src/main/java/me/nandork/simple/parser/IndexHuArticleParser.java
+++ src/main/java/me/nandork/simple/parser/IndexHuArticleParser.java
@@ -1,11 +1,14 @@
 package me.nandork.simple.parser;
 
 import me.nandork.simple.model.Article;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,9 +20,21 @@ public class IndexHuArticleParser implements ArticleParser {
     public List<Article> apply(Element body) {
         Elements articles = body.select("article");
         return articles.stream().map(article -> {
-            String title = body.select("a").text();
-            String text = body.select("span").text();
+            String title = article.select(".cikkcim").text();
+            String text = article.select("span").text();
             return new Article(title, text);
         }).collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.connect("http://index.hu").get();
+
+        Element body = doc.body();
+
+        List<Article> apply = new IndexHuArticleParser().apply(body);
+
+        for (Article article : apply) {
+            System.out.println(article.getTitle());
+        }
     }
 }
