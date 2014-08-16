8b26632939ae96a2d3ec4c95bf39f2eb115a00b8
Nandor Kracser <bonifaido@gmail.com>
2014-08-16 22:04:37 +0200

    removed test main

--- src/main/java/me/nandork/simple/parser/IndexHuArticleParser.java
+++ src/main/java/me/nandork/simple/parser/IndexHuArticleParser.java
@@ -1,14 +1,11 @@
 package me.nandork.simple.parser;
 
 import me.nandork.simple.model.Article;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,18 +23,5 @@ public class IndexHuArticleParser implements ArticleParser {
             String href = cikkcim.select("a").first().attr("href");
             return new Article(title, text, href);
         }).collect(Collectors.toList());
-    }
-
-    public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.connect("http://index.hu").get();
-
-        Element body = doc.body();
-
-        List<Article> articles = new IndexHuArticleParser().apply(body);
-
-        for (Article article : articles) {
-            String href = article.getHref();
-            System.out.println(href);
-        }
     }
 }
