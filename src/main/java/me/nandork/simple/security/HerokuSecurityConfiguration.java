c6a72a90867751c7ceab3addd12f77e6d3827ee4
Nandor Kracser <bonifaido@gmail.com>
2014-08-19 23:12:45 +0200

    using basic instead of form authentication - going back to basic

--- src/main/java/me/nandork/simple/security/HerokuSecurityConfiguration.java
+++ src/main/java/me/nandork/simple/security/HerokuSecurityConfiguration.java
@@ -4,20 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class HerokuSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    HerokuUserDetailsAuthenticationProvider herokuUserDetailsAuthenticationProvider;
+    private HerokuUserDetailsAuthenticationProvider herokuUserDetailsAuthenticationProvider;
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .anyRequest().authenticated().and()
-//                .httpBasic();
-//    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated().and()
+                .httpBasic();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
