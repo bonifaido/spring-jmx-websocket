54a54ed1e856f6bdef34633a776cd3682cc3d7af
Nandor Kracser <bonifaido@gmail.com>
2014-08-19 22:50:32 +0200

    using basic instead of form authentication

--- src/main/java/me/nandork/simple/security/HerokuSecurityConfiguration.java
+++ src/main/java/me/nandork/simple/security/HerokuSecurityConfiguration.java
@@ -3,6 +3,7 @@ package me.nandork.simple.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -10,6 +11,13 @@ public class HerokuSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     HerokuUserDetailsAuthenticationProvider herokuUserDetailsAuthenticationProvider;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated().and()
+                .httpBasic();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
