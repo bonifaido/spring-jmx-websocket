6f2f0a4a595106cc3ba8c7bd3b5689ade02333e9
Nandor Kracser <bonifaido@gmail.com>
2014-08-20 13:21:13 +0200

    added missing files

---
+++ src/main/java/me/nandork/simple/security/AppSecurityConfiguration.java
@@ -0,0 +1,30 @@
+package me.nandork.simple.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(new InMemoryUserDetailsManager(Arrays.asList(new User("nundi", "nundi01", AuthorityUtils.createAuthorityList("USER")))));
+    }
+}
